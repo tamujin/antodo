2651cc8f8ee7208d712245b132992cc9a5ef600e
Willi <mail2waleed@yahoo.com>
2017-07-06 15:54:40 +0200

    new file strct

--- app/mock-todo.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=mock-todo.js.map
--- app/todo.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var todo = (function () {
-    function todo() {
-    }
-    return todo;
-}());
-exports.todo = todo;
-//# sourceMappingURL=todo.js.map
